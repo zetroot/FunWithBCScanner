c13a3375b942b4580d6f4915305f066d8b591d60
a.sashurin <a.sashurin@amplitech.ru>
2020-07-31 10:02:08 +0300

    Add project files.

---
+++ FunWithRS232/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunWithRS232
+{
+    class Program
+    {
+
+        private static SerialPort GetPort()
+        {
+            var ports = SerialPort.GetPortNames().Select((x, i) => (i, x)).ToDictionary(x => x.i, x => x.x);
+            Console.WriteLine("Found ports:");
+            foreach (var p in ports)
+                Console.WriteLine($"{p.Key}\t{p.Value}");
+            Console.Write("What port do u wanna to use: ");
+
+            var portKey = Console.ReadKey() switch
+            {
+                ConsoleKeyInfo ki when char.IsDigit(ki.KeyChar) => int.Parse(ki.KeyChar.ToString()),
+                _ => -1
+            };
+            Console.WriteLine();
+            if (portKey == -1) return null;
+            return new SerialPort(ports[portKey], 115200, Parity.None, 8, StopBits.One);
+        }
+
+        static async Task Main(string[] args)
+        {
+            var reader = new ScannerReader(new ScannerOptions { PortName = "COM3", BaudRate = 115200, DataBits = 8, Parity = Parity.None, StopBits = StopBits.One });
+
+            await reader.ObserveScanner();
+        }
+    }
+}
---
+++ FunWithRS232/ScannerReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithRS232
+{
+    public class ScannerOptions
+    {
+        public string PortName { get; set; }
+        public int BaudRate { get; set; }
+        public Parity Parity { get; set; } 
+        public int DataBits { get; set; }
+        public StopBits StopBits { get; set; }
+    }
+
+    public class ScannerReader
+    {
+        private readonly ScannerOptions options;
+
+        public ScannerReader(ScannerOptions options)
+        {
+            this.options = options;
+        }
+
+        private readonly List<char> globalBuffer = new List<char>(65536);
+
+        public IObservable<string> Barcodes { get; }
+
+        public async Task ObserveScanner()
+        {
+            var port = new SerialPort(options.PortName, options.BaudRate, options.Parity, options.DataBits, options.StopBits);
+            port.Open();
+
+            var stream = port.BaseStream;
+            using var sreader = new StreamReader(stream);
+            using var owner = MemoryPool<char>.Shared.Rent();
+            while (true)
+            {
+                var buf = new char[4096];
+
+                var readBytes = await sreader.ReadAsync(buf, 0, buf.Length);
+                globalBuffer.AddRange(buf.Take(readBytes));
+                Console.WriteLine($"Read {readBytes} bytes");
+                for (int i = 0; i < readBytes; ++i)
+                    Console.WriteLine($"{buf[i]}\t{((byte)buf[i]).ToString("X2")}");
+                Console.WriteLine($"now global buffer is {globalBuffer.Count} elements");
+            }
+        }
+    }
+}
